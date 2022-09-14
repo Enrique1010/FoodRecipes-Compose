eecf14fe4e7b51fa17cc9087c6ca68c0a8a54613
Luis Enrique Ramirez <luis_enrique_ramirez@claro.com.do>
2022-09-14 14:03:48 -0400

    initial commit

---
+++ app/src/main/java/com/erapps/foodrecipesapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.erapps.foodrecipesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.erapps.foodrecipesapp.ui.theme.FoodRecipesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodRecipesAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
